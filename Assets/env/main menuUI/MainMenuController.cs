2483f617a502a9c0ae7a217d44278fb877534dd0
叉滴滴 <kstapryun1101107@gmail.com>
2024-07-09 00:59:05 +0800

    main menu settings

---
+++ Assets/env/main menuUI/MainMenuController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Timeline.Actions;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class NewBehaviourScript : MonoBehaviour
+{
+   public CanvasGroup OptionPanel;
+
+   public void PLayGame()
+   {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
+   }
+   public void Option()
+   {
+        OptionPanel.alpha = 1;
+        OptionPanel.blocksRaycasts = true;
+   }
+   public void Back()
+   {
+        OptionPanel.alpha = 0;
+        OptionPanel.blocksRaycasts = false;
+   }
+
+   public void QuitGame()
+   {
+    Application.Quit();
+   }
+}
