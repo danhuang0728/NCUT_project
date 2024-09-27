2f2a58663c0d4ce43004f963207254fef6b33c2f
danhuang <danhuang0728@gmail.com>
2024-09-28 02:17:33 +0800

    修正腳本BUG
    SceneManager.LoadScene("SampleScene");

--- Assets/env/Scripts/MainMenuController.cs
+++ Assets/env/Scripts/MainMenuController.cs
@@ -11,7 +11,7 @@ public class NewBehaviourScript : MonoBehaviour
 
    public void PLayGame()
    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
+        SceneManager.LoadScene("SampleScene");
    }
    public void Option()
    {
