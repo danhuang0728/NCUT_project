9b92b6d7eebe3eab0664dcf17950d453ddfe7d73
danhuang <danhuang0728@gmail.com>
2024-10-26 02:28:40 +0800

    新增倒數結束會清除怪物並且關閉怪物重生點

--- Assets/env/Scripts/LevelTrigger.cs
+++ Assets/env/Scripts/LevelTrigger.cs
@@ -7,17 +7,20 @@ public class LevelTrigger : MonoBehaviour
 {
     public Collider2D collider2d;
     public LayerMask player;
-    public int levelminTime;
-    public int levelmaxTime;
+    public LayerMask monsterlayer;
+    public int levelminTime = 180;
+    public int levelmaxTime = 300;
 
     private int levelTime = 0;
     private TrapControll[] trapControlls;  //一次抓取全部linktrap的TrapControll腳本
+    private spawn[] spawns; 
 
     void Start()
     {
         
         collider2d = GetComponent<Collider2D>(); 
         trapControlls = FindObjectsOfType<TrapControll>(); //一次抓取全部linktrap的物件
+        spawns = FindObjectsOfType<spawn>();
     }
 
     void Update()
@@ -42,6 +45,12 @@ public class LevelTrigger : MonoBehaviour
     IEnumerator levelstart(int leveltime){  //leaveltime為房間的運作時間
         
         yield return new WaitForSeconds(leveltime); 
+        ClearAllClones(); 
+
+        foreach (spawn repOb in spawns) //修改全部重生點為關閉狀態
+            {
+                repOb.enabled = false;
+            }
         foreach (TrapControll trap in trapControlls) //修改全部trap物件裡的的bool為true
             {
                 Animator trapAni = trap.GetComponent<Animator>();
@@ -51,8 +60,23 @@ public class LevelTrigger : MonoBehaviour
                 trapAni.Play("TarpAni_single", 0, 0f);
                 trapAni.Update(0);
                 //--------------------------
+
                 trap.close = false;  
             }
       
+    }
+    public void ClearAllClones()   //清除複製物件
+    {
+        // 獲取所有場景中的物件
+        GameObject[] allObjects = GameObject.FindObjectsOfType<GameObject>();
+
+        // 遍歷所有物件，查找名稱包含 "Clone" 的物件
+        foreach (GameObject obj in allObjects)
+        {
+            if (obj.name.Contains("Clone")) // 替換為適當的名稱檢查
+            {
+                Destroy(obj);
+            }
+        }
     }
 }
