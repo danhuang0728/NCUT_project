aacc708c4543ddbd2e0ab638c357bd4ee8762b8d
penis <danhuang0728@gmail.com>
2025-02-06 17:50:43 +0800

    2/6 主檔案 新增武器管理腳本

---
+++ Assets/env/Scripts/Weapon Scripts/Weapon_Manager.cs
@@ -0,0 +1,95 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon_Manager : MonoBehaviour
+{
+    // 圓形武器：包含啟用開關與等級設定
+    public bool circle = false;
+    [Range(1, 5)]
+    public int circle_level = 1;
+    GameObject circle_weapon;
+    GearWeaponSystem circle_weapon_system;
+    
+    // 回旋標武器：包含啟用開關與等級設定
+    public bool Boomerang = false;
+    [Range(1, 6)]
+    public int boomerang_level = 1;
+    GameObject boomerang_weapon;
+    BoomerangController boomerang_controller;
+    // 魔法書武器：包含啟用開關與等級設定
+    public bool MagicBook = false;
+    [Range(1, 7)]
+    public int magicbook_level = 1;
+    GameObject magicbook_weapon;
+    MagicBook magicbook_script;
+
+    // 西洋劍武器：包含啟用開關與等級設定
+    public bool thrust = false;
+    [Tooltip("等級系統尚未完成")]
+    [Range(1, 7)]
+    public int thrust_level = 1;
+    GameObject thrust_weapon;
+
+
+    // 斧頭武器：包含啟用開關與等級設定
+    public bool Axe = false;
+    [Tooltip("等級系統尚未完成")]
+    [Range(1, 7)]
+    public int axe_level = 1;
+    GameObject axe_weapon;
+
+
+
+
+    // 弩箭砲台武器：包含啟用開關與等級設定
+    public bool crossbow = false;
+    [Tooltip("等級系統尚未完成")]
+    [Range(1, 7)]
+    public int crossbow_level = 1;
+    GameObject crossbow_weapon;
+    
+    
+
+
+
+
+    void Start()
+    {
+        circle_weapon = transform.Find("circle").gameObject;
+        boomerang_weapon = transform.Find("Boomerang").gameObject;
+        magicbook_weapon = transform.Find("MagicBook").gameObject;
+        thrust_weapon = transform.Find("thrust").gameObject;
+        axe_weapon = transform.Find("Axe_Slashh_0").gameObject;
+        crossbow_weapon = transform.Find("crossbow").gameObject;
+        circle_weapon_system = circle_weapon.GetComponent<GearWeaponSystem>();
+        boomerang_controller = boomerang_weapon.GetComponent<BoomerangController>();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        setActiveWeapon(circle, circle_weapon);
+        setActiveWeapon(Boomerang, boomerang_weapon);
+        setActiveWeapon(MagicBook, magicbook_weapon);
+        setActiveWeapon(thrust, thrust_weapon);
+        setActiveWeapon(Axe, axe_weapon);
+        setActiveWeapon(crossbow, crossbow_weapon);
+        circle_weapon_system.level = circle_level;
+        boomerang_controller.level = boomerang_level;
+        magicbook_script.level = magicbook_level;
+
+    }
+    void setActiveWeapon(bool weapon, GameObject weapon_object)
+    {
+        if(weapon)
+        {
+            weapon_object.SetActive(true);
+        }
+        else
+        {
+            weapon_object.SetActive(false);
+        }
+    }
+}
