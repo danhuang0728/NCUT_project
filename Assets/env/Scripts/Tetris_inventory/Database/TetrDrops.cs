e7bc569715baccaa50084019dc57ec79146d289c
danhuang <danhuang0728@gmail.com>
2025-04-12 17:47:02 +0800

    4/12 主檔案 能力方塊拾取物件預置物模板
    補上金幣消耗參數

--- Assets/env/Scripts/Tetris_inventory/Database/TetrDrops.cs
+++ Assets/env/Scripts/Tetris_inventory/Database/TetrDrops.cs
@@ -5,13 +5,16 @@ using UnityEngine;
 public class TetrDrops : MonoBehaviour
 {
     public tetr_database tetr_database;
+    public int gold_cost; // 花費金幣
     private player_tetr_Manager player_tetr_Manager;
+    private character_value_ingame Character_value_ingame;
     private SpriteRenderer spriteRenderer;
     void Start()
     {
         player_tetr_Manager = FindObjectOfType<player_tetr_Manager>();
         spriteRenderer = GetComponent<SpriteRenderer>();
         spriteRenderer.sprite = tetr_database.tetr_sprite;
+        Character_value_ingame = FindObjectOfType<character_value_ingame>();
     }
 
     // Update is called once per frame
@@ -23,8 +26,16 @@ public class TetrDrops : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Player")&&Input.GetKeyDown(KeyCode.E))
         {
-            player_tetr_Manager.add_tetr_obj(tetr_database);
-            Destroy(gameObject);
+            if (Character_value_ingame.gold >= gold_cost)
+            {
+                player_tetr_Manager.add_tetr_obj(tetr_database);
+                Character_value_ingame.gold -= gold_cost;
+                Destroy(gameObject);
+            }
+            else
+            {
+                Debug.Log("金幣不足");
+            }
         }
     }
 }
