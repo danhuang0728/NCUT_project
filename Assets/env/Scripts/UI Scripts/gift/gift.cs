7e1588f769413e86030f35e150d24da5d295721e
danhuang <danhuang0728@gmail.com>
2025-04-20 19:04:23 +0800

    4/20 天賦頁面完成

--- Assets/env/Scripts/UI Scripts/gift/gift.cs
+++ Assets/env/Scripts/UI Scripts/gift/gift.cs
@@ -1,31 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class gift : MonoBehaviour
 {
-    public Slider slider; // 滑桿物件
+    private Slider slider; // 滑桿物
+    public float giftValue;
     public Character_Values_SETUP character_Values_SETUP; // 角色數值腳本
+    private TextMeshProUGUI gift_point;
     #if UNITY_EDITOR
     [StringDropdown("damage", "criticalDamage", "criticalHitRate", "speed", "health", "cooldown", "lifeSteal")] // 根据实际关卡设计修改选项
     #endif
     [SerializeField] private string selectedOption = "damage"; // 设置默认值
-    public float effect_addition; // 效果加成值
+    private float effect_addition; // 效果加成值
     void Start()
     {
-        
+        gift_point = GameObject.Find("point_Value").GetComponent<TextMeshProUGUI>();
+        //抓取子物件slider
+        slider = GetComponentInChildren<Slider>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        gift_point.text = character_Values_SETUP.GIFT_Value.ToString();
+        OnSliderValueChanged(slider.value, selectedOption);
     }
     public void OnSliderValueChanged(float value, string selectedOption)
     {
         // 更新效果加成值
-        effect_addition = value; 
+        effect_addition = (value - 1) * 10; 
 
         if (selectedOption == "damage")
         {
@@ -59,14 +66,22 @@ public class gift : MonoBehaviour
 
     public void add_buttonclick()
     {
-        // 獲取滑桿的當前值
-        float sliderValue = slider.value;
-        slider.value ++;
+        if(slider.value < slider.maxValue && character_Values_SETUP.GIFT_Value > 0)
+        {
+            // 獲取滑桿的當前值
+            float sliderValue = slider.value;
+            slider.value ++;
+            character_Values_SETUP.GIFT_Value --;
+        }
     }
     public void minus_buttonclick()
     {
-        // 獲取滑桿的當前值
-        float sliderValue = slider.value;
-        slider.value --;
+        if(slider.value > slider.minValue)
+        {
+            // 獲取滑桿的當前值
+            float sliderValue = slider.value;
+            slider.value --;
+            character_Values_SETUP.GIFT_Value ++;
+        }
     }
 }
