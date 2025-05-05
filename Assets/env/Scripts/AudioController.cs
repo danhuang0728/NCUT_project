e6536ddbecf75cbdd87e3399b60aa766e1004bff
danhuang <danhuang0728@gmail.com>
2025-05-05 18:42:02 +0800

    5/5 Demo ver 0.0.3
    修復音效無法調整

--- Assets/env/Scripts/AudioController.cs
+++ Assets/env/Scripts/AudioController.cs
@@ -17,27 +17,27 @@ public class AudioController : MonoBehaviour
     }
     public void MusicVolume()
     {
-        AudioManager.Instance.MusicVolume(_musicSlider.value);
+        AudioManager.Instance.MusicVolume((_musicSlider.value + 0.25f)/10);
     }
     public void SFXVolume()
     {
-        AudioManager.Instance.SFXVolume(_sfxSlider.value);
+        AudioManager.Instance.SFXVolume((_sfxSlider.value + 0.25f)/10);
     }
     void Start()
     {
-    // 初始音量值 (預設為 100%)
-    float defaultVolume = 1.0f; 
+        // 初始音量值 (預設為 50%)
+        float defaultVolume = 0.5f; 
 
-    // 加載保存的音量值，若無則使用預設值
-    float savedMusicVolume = PlayerPrefs.GetFloat("MusicVolume", defaultVolume);
-    float savedSFXVolume = PlayerPrefs.GetFloat("SFXVolume", defaultVolume);
+        // 加載保存的音量值，若無則使用預設值
+        float savedMusicVolume = PlayerPrefs.GetFloat("MusicVolume", defaultVolume);
+        float savedSFXVolume = PlayerPrefs.GetFloat("SFXVolume", defaultVolume);
 
-    // 設定 Slider 的初始值
-    _musicSlider.value = savedMusicVolume;
-    _sfxSlider.value = savedSFXVolume;
+        // 設定 Slider 的初始值
+        _musicSlider.value = savedMusicVolume*10;
+        _sfxSlider.value = savedSFXVolume*10;
 
-    // 同時將音量應用到 AudioManager
-    AudioManager.Instance.MusicVolume(savedMusicVolume);
-    AudioManager.Instance.SFXVolume(savedSFXVolume);
+        // 同時將音量應用到 AudioManager
+        AudioManager.Instance.MusicVolume(savedMusicVolume);
+        AudioManager.Instance.SFXVolume(savedSFXVolume);
     }
 }
