9af4d413a4ce6aa36432a54827323a2af7ef15fc
danhuang <danhuang0728@gmail.com>
2025-05-21 01:02:19 +0800

    Demo ver 0.2.6
    修復小花腳本

--- Assets/env/Scripts/Boss_Scripts/BossFlower.cs
+++ Assets/env/Scripts/Boss_Scripts/BossFlower.cs
@@ -11,7 +11,6 @@ public class BossFlower : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        bossFloat = FindObjectOfType<BossFloat>();
         
     }
 
