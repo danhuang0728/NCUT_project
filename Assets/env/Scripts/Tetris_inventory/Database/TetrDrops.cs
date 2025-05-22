9a602102e49c1d75389d347b02aae0fd8185feb5
danhuang <danhuang0728@gmail.com>
2025-05-22 19:04:47 +0800

    5/22 加小房間

--- Assets/env/Scripts/Tetris_inventory/Database/TetrDrops.cs
+++ Assets/env/Scripts/Tetris_inventory/Database/TetrDrops.cs
@@ -7,6 +7,7 @@ public class TetrDrops : MonoBehaviour
     public tetr_database tetr_database;
     public int gold_cost; // 花費金幣
     private player_tetr_Manager player_tetr_Manager;
+    private GameObject player;
     private character_value_ingame Character_value_ingame;
     private SpriteRenderer spriteRenderer;
     void Start()
@@ -15,16 +16,13 @@ public class TetrDrops : MonoBehaviour
         spriteRenderer = GetComponent<SpriteRenderer>();
         spriteRenderer.sprite = tetr_database.tetr_sprite;
         Character_value_ingame = FindObjectOfType<character_value_ingame>();
+        player = FindObjectOfType<PlayerControl>().gameObject;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
-    void OnTriggerStay2D(Collider2D other)
-    {
-        if (other.gameObject.CompareTag("Player")&&Input.GetKeyDown(KeyCode.E))
+        if (Vector2.Distance(transform.position, player.transform.position) < 1f && Input.GetKeyDown(KeyCode.E))
         {
             if (Character_value_ingame.gold >= gold_cost)
             {
@@ -37,5 +35,10 @@ public class TetrDrops : MonoBehaviour
                 Debug.Log("金幣不足");
             }
         }
+    } 
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, 1f);
     }
 }
